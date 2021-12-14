be672ac3c2d9b3d84069f5a5f44a7d0053f88bd2
DanieleFic <reb_ficus22@hotmail.it>
2021-12-14 19:36:20 +0100

    milestone 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,4 @@
-const items = [
+const ImmaginiArr = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -22,31 +22,65 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-let itemsContent =  '';
-let elementActive = 0;
+let immagineMain =  ''; //creiamo una variabile let(perchè il contenuto cambia) vuota che poi andremo a riempire
+let elementActive = 0;  // creiamo una variabile e gli diamo 0 per renderla false che serve per rende le nostre img nascoste
 
-for (let i = 0; i< items.length; i++){
-    itemsContent += `
+for (let i = 0; i< ImmaginiArr.length; i++){ //creiamo un ciclo for che va a riempire il contenuto della nostra variabile immagineMain per ogni volta fino a quando la funzione .lenght lo rende possibile 
+    immagineMain += `
         <div class="item" id="item-${i}">
-            <img src="${items[i]}">
+            <img src="${ImmaginiArr[i]}">
         </div>`;
 }
 
-const itemsContainer = document.querySelector ('.container');
-itemsContainer.innerHTML = itemsContent;
+const contenitoreMain = document.querySelector ('.container');
+contenitoreMain.innerHTML = immagineMain;
 
-const imgActive = document.getElementById('item-'+elementActive)
+let imgActive = document.getElementById('item-' + elementActive) 
 imgActive.classList.add('active');
+//fine immagine principale 
 
 
-let itemsSlide =  '';
+let immaginiLaterali =  ''; //creiamo una variabile let(perchè il contenuto cambia) vuota che poi andremo a riempire
 
-for (let i = 0; i< items.length; i++){
-    itemsSlide += `
+for (let i = 0; i< ImmaginiArr.length; i++){ //creiamo un ciclo for che va a riempire il contenuto della nostra variabile immagineMain per ogni volta fino a quando la funzione .lenght lo rende possibile 
+    immaginiLaterali += `
         <div class="items" id="item-${i}">
-            <img src="${items[i]}">
+            <img src="${ImmaginiArr[i]}">
         </div>`;
 }
 
-const itemsContainer2 = document.querySelector ('.ms_side');
-itemsContainer2.innerHTML = itemsSlide;
+const contenitoreSide = document.querySelector ('.ms_side');
+contenitoreSide.innerHTML += immaginiLaterali;
+//fine immagine principale 
+
+
+
+//let testoMain =  ''; //creiamo una variabile let(perchè il contenuto cambia) vuota che poi andremo a riempire
+
+//for (let i = 0; i< title.length; i++){ //creiamo un ciclo for che va a riempire il contenuto della nostra variabile immagineMain per ogni volta fino a quando la funzione .lenght lo rende possibile 
+//    testoMain += `
+//        <h3 class="titolo" id="titolo1-${i}">
+//            
+//        </h3>`;
+//}
+
+//const contenitoreTesto = document.querySelector ('.titolo');
+//contenitoreTesto.innerHTML =+ testoMain;
+
+//non funziona
+const up = document.getElementById ('.ms_up');
+const down = document.getElementById ('.ms_down');
+
+up.addEventListener('click',function(){
+    elementActive++;
+    imgActive.classList.remove('active');
+    imgActive = getElementById('item-'+ elementActive);
+    imgActive.classList.add('active')
+});
+
+down.addEventListener('click',function(){
+    elementActive--;
+    imgActive.classList.remove('active');
+    imgActive = getElementById('item-'+ elementActive);
+    imgActive.classList.add('active')
+});
